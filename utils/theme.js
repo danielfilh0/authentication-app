df90a5fd410673a5b4e18a80f549a3a0d4ff878b
Daniel Filho <danbsilva98@gmail.com>
2022-08-05 15:17:10 -0300

    initial commit

---
+++ utils/theme.js
@@ -0,0 +1,26 @@
+import colors from 'vuetify/es5/util/colors'
+
+export const theme = {
+  dark: {
+    primary: '#2F80ED', // azul
+    secondary: '#E0E0E0', // cinza claro
+    gray: '#828282', // cinza médio
+    background: '#252329',
+    accent: colors.grey.darken3,
+    info: colors.teal.lighten1,
+    warning: colors.amber.base,
+    error: colors.deepOrange.accent4,
+    success: colors.green.accent3,
+  },
+  light: {
+    primary: '#2F80ED;', // azul
+    secondary: '#333333', // cinza escuro
+    gray: '#828282', // cinza médio
+    background: '#FFFFFF',
+    accent: colors.grey.darken3,
+    info: colors.teal.lighten1,
+    warning: colors.amber.base,
+    error: colors.deepOrange.accent4,
+    success: colors.green.accent3,
+  },
+}
