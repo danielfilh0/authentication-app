f46571bc5ba4296b14600d8d6da9190ada24f33a
Daniel Filho <danbsilva98@gmail.com>
2022-10-06 19:30:56 -0300

    feat: change favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,14 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - authentication-app',
-    title: 'authentication-app',
+    title: 'Authentication App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/devchallenges.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
