159b36171d04457dd6e2ffadebccf901ae0246c7
Daniel Filho <danbsilva98@gmail.com>
2022-10-07 18:55:53 -0300

    refactor: change environment variables names

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: process.env.NUXT_ENV_VERCEL_FIREBASE_API_KEY,
+  authDomain: process.env.NUXT_ENV_VERCEL_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NUXT_ENV_VERCEL_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NUXT_ENV_VERCEL_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NUXT_ENV_VERCEL_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NUXT_ENV_VERCEL_FIREBASE_APP_ID
 }
 
 const firebase = initializeApp(firebaseConfig)
